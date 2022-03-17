7d5f8a0067f113c3c8e15c4234b200dead7bff3e
Christian Ruf <c.ruf99@gmail.com>
2022-03-17 16:03:12 +0100

    Fix import

--- channelchat/events/handlers/__init__.py
+++ channelchat/events/handlers/__init__.py
@@ -1,11 +1,11 @@
 from importlib import import_module
-from pkgutil import walk_packages, iter_modules
+from pkgutil import iter_modules
 
-from channelchat import events
+import channelchat
 
 
 def init():
-    package_infos = iter_modules(events.handlers.__path__, prefix='events.handlers.')
+    package_infos = iter_modules(channelchat.events.handlers.__path__, prefix='channelchat.events.handlers.')
     for info in package_infos:
         if info.ispkg: continue
         import_module(info.name)
